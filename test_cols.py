87ede05001d2a8f4706375c5e08e0ebe345f26f3
Dayat <dayat@dev.local>
2026-05-13 15:55:49 +0700

    filter  review rating sdnegri

--- test_cols.py
+++
@@ -1,8 +0,0 @@
-import sys
-sys.path.append('.')
-from dashboard.db_access import get_cursor
-with get_cursor() as cur:
-    cur.execute("SELECT column_name FROM information_schema.columns WHERE table_name='portal_kelurahan'")
-    print([r['column_name'] for r in cur.fetchall()])
-    cur.execute("SELECT column_name FROM information_schema.columns WHERE table_name='portal_kecamatan'")
-    print([r['column_name'] for r in cur.fetchall()])
